7e681562427cee555f6fdc1ad88d6c731e01e5b0
DF2002XD <58273496+DF2002XD@users.noreply.github.com>
2023-09-27 10:27:47 +0200

    Add files via upload

---
+++ Configuración_Apache/Conexion.php
@@ -0,0 +1,34 @@
+<?php
+
+
+function ConexionBD( $server = "192.168.204.111", $bd = "OnixInteco",$un = "sa", $ps = "Iiaslgv52d")
+{
+    //Variables que sacaremos de la BBDD de wordpress
+            $bbdd = $bd;
+            $username = $un;
+            $pass = $ps;
+            $server = $server;
+    try {
+  
+         $connectionInfo = array(
+            "Database" => $bbdd,
+             "UID" => $username,
+              "PWD" => $pass,
+               "CharacterSet" => "UTF-8",
+             "ConnectionPooling" => "1",
+              "MultipleActiveResultSets" => '0',
+              "Encrypt" => false
+              );
+        $conn = sqlsrv_connect($server, $connectionInfo);
+        if( $conn === false ) {
+     		die( print_r( sqlsrv_errors(), true));
+	}
+        //$conn = new PDO ("sqlsrv:Server=$server,$puerto;Database=$bbdd",$username,$pass);
+    } catch (EXCEPTION $e) {
+        echo ("No se logró conectar correctamente con la base de datos: $bbdd, error: $e");
+    }
+    
+    return $conn;
+}
+?>
+
